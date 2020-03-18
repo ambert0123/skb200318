d5a2dffa9040fb035f90469c5ed1ad82ef673d60
Will <doggy.huang@gmail.com>
2020-03-18 17:16:33 +0800

    刪除沒用的 UtilitiesComponent

--- src/app/utilities/utilities-routing.module.ts
+++ src/app/utilities/utilities-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { UtilitiesComponent } from './utilities.component';
+const routes: Routes = [
 
-const routes: Routes = [{ path: '', component: UtilitiesComponent }];
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/utilities/utilities.module.ts
+++ src/app/utilities/utilities.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UtilitiesRoutingModule } from './utilities-routing.module';
-import { UtilitiesComponent } from './utilities.component';
 
 
 @NgModule({
-  declarations: [UtilitiesComponent],
+  declarations: [
+
+  ],
   imports: [
     CommonModule,
     UtilitiesRoutingModule
