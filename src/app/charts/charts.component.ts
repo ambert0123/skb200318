c4d80f9120386b2b1e4e4cc6a43fb0f2a189654f
Will <doggy.huang@gmail.com>
2020-03-18 15:24:55 +0800

    示範路由參數寫法

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { chartAreaDemo } from '../chartAreaDemo';
 import { chartPieDemo } from '../chartPieDemo';
 import { chartBarDemo } from '../chartBarDemo';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-charts',
@@ -10,9 +11,15 @@ import { chartBarDemo } from '../chartBarDemo';
 })
 export class ChartsComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  type = '';
+
+  constructor(private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(param => {
+      this.type = param.get('type');
+    });
   }
 
   ngAfterViewInit(): void {
@@ -21,5 +28,10 @@ export class ChartsComponent implements OnInit, AfterViewInit {
       chartPieDemo();
       chartBarDemo();
     }, 0);
+  }
+
+  plusOne(value: string) {
+    const num = +value + 1;
+    this.router.navigateByUrl('/charts/' + num);
   }
 }
