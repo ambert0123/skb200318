5bbace581b9f9cb7fd4a2759a5c60fa797170d42
Will <doggy.huang@gmail.com>
2020-03-25 14:47:43 +0800

    加入 Login2 元件與匯入 ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +14,7 @@ import { ChartsComponent } from './charts/charts.component';
 import { TablesComponent } from './tables/tables.component';
 import { LoginComponent } from './login/login.component';
 import { LayoutComponent } from './layout/layout.component';
+import { Login2Component } from './login2/login2.component';
 
 @NgModule({
   declarations: [
@@ -25,13 +26,15 @@ import { LayoutComponent } from './layout/layout.component';
     ChartsComponent,
     TablesComponent,
     LoginComponent,
-    LayoutComponent
+    LayoutComponent,
+    Login2Component
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     CalendarModule
   ],
   providers: [],
