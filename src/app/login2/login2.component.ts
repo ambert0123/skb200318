7d832251fd0329ba32e179f70a1ceed1a8646bc6
Will <doggy.huang@gmail.com>
2020-03-25 14:58:33 +0800

    示範 Reactive Forms 設計與套版方法

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login2',
@@ -9,15 +10,29 @@ export class Login2Component implements OnInit, OnDestroy {
 
   origBodyClass = '';
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.origBodyClass = document.body.className;
     document.body.className = 'bg-gradient-primary';
+
+    this.form = this.fb.group({
+      email: 'doggy.huang@gmail.com',
+      password: '123',
+      isRemember: true
+    });
+
   }
 
   ngOnDestroy(): void {
     document.body.className = this.origBodyClass;
   }
 
+  doSubmit() {
+    if (this.form.valid) {
+      // TODO
+    }
+  }
 }
